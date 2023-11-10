3937ca6389fc0c5c69638dd4e30b97521b42c496
nate-crema <nate.crema@gmail.com>
2023-11-10 17:14:53 +0900

    Update: 사용자가 연속적으로 입력할 때, 입력이 완료된 뒤 입력값이 전달되도록 수정
    * 입력시간을 저장하는 ref를 추가하여, 사용자의 연속입력 감지 및 연속입력이 완료되었을 때 상위 컴포넌트로 값이 전달되도록 수정

--- src/components/molecule/InputBlock/index.jsx
+++ src/components/molecule/InputBlock/index.jsx
@@ -1,12 +1,14 @@
 import { 
     useState, useEffect,
     HTMLInputTypeAttribute,
-    HTMLAttributes, HTMLDivElement
+    HTMLAttributes, HTMLDivElement, useRef
 } from "react";
 
 import * as S from "./style";
 
 import { SERVICE_BLOCK_TYPE } from "../../atom/ServiceBlock/constant";
+
+import util from "../../../util";
 
 /**
  * 
@@ -28,11 +30,19 @@ const InputBlock = ({
     ...props
 }) => {
 
+    const recent_input_ts = useRef();
     const [ value, setValue ] = useState( init_value || "" );
 
-    useEffect(() => {
+    useEffect(() => { ( async () => {
+        // 연속입력 시 입력이 종료된 이후에 데이터가 한번에 전달되도록 설정
+        const now_ts = new Date().getTime();
+        recent_input_ts.current = now_ts;
+
+        await util.sleep( 300 );
+        if ( recent_input_ts.current !== now_ts ) return;
+
         if ( onValueChange ) onValueChange( value );
-    }, [ value ]);
+    } )() }, [ value ]);
 
     const onInputChange = ( e ) => {
         setValue( e.target.value );
